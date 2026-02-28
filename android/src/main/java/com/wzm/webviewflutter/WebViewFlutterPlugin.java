5261c4d9174fe0d53ad39c3fe82245c94c6da9e7
laibin <laibin@trendit.com>
2026-02-28 14:33:55 +0800

    支持flutter3.41

--- android/src/main/java/com/wzm/webviewflutter/WebViewFlutterPlugin.java
+++ android/src/main/java/com/wzm/webviewflutter/WebViewFlutterPlugin.java
@@ -5,25 +5,19 @@
 package com.wzm.webviewflutter;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 
 import android.util.Log;
-import io.flutter.app.FlutterApplication;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * Java platform implementation of the webview_flutter plugin.
  *
  * <p>Register this in an add to app scenario to gracefully handle activity and context changes.
- *
- * <p>Call {@link #registerWith(Registrar)} to use the stable {@code io.flutter.plugin.common}
- * package instead.
  */
 public class WebViewFlutterPlugin implements FlutterPlugin , PluginRegistry.ActivityResultListener,PluginRegistry.RequestPermissionsResultListener, ActivityAware{
   private static final String TAG = "WebViewFlutterPlugin";
@@ -35,32 +29,10 @@ public class WebViewFlutterPlugin implements FlutterPlugin , PluginRegistry.Acti
    * Add an instance of this to {@link io.flutter.embedding.engine.plugins.PluginRegistry} to
    * register it.
    *
-   * <p>THIS PLUGIN CODE PATH DEPENDS ON A NEWER VERSION OF FLUTTER THAN THE ONE DEFINED IN THE
-   * PUBSPEC.YAML. Text input will fail on some Android devices unless this is used with at least
-   * flutter/flutter@1d4d63ace1f801a022ea9ec737bf8c15395588b9. Use the V1 embedding with {@link
-   * #registerWith(Registrar)} to use this plugin with older Flutter versions.
-   *
    * <p>Registration should eventually be handled automatically by v2 of the
    * GeneratedPluginRegistrant. https://github.com/flutter/flutter/issues/42694
    */
   public WebViewFlutterPlugin() {}
-
-  /**
-   * Registers a plugin implementation that uses the stable {@code io.flutter.plugin.common}
-   * package.
-   *
-   * <p>Calling this automatically initializes the plugin. However plugins initialized this way
-   * won't react to changes in activity or context, unlike {CameraPlugin}.
-   */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    registrar
-        .platformViewRegistry()
-        .registerViewFactory(
-            "plugins.flutter.io/webview",
-            new WebViewFactory(registrar.messenger(), registrar.view()));
-    new FlutterCookieManager(registrar.messenger());
-    }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
@@ -71,14 +43,6 @@ public class WebViewFlutterPlugin implements FlutterPlugin , PluginRegistry.Acti
         .registerViewFactory(
             "plugins.flutter.io/webview", factory);
     flutterCookieManager = new FlutterCookieManager(messenger);
-
-    Context appContext = binding.getApplicationContext();
-    if (appContext instanceof FlutterApplication) {
-      Activity currentActivity = ((FlutterApplication) appContext).getCurrentActivity();
-      if (currentActivity != null) {
-        activity = currentActivity;
-      }
-    }
   }
 
   @Override
